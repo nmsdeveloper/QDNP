cf9add8f6116a091b2401b1c2e9e57e12ea6bece
sam31403 <samuel.mouango@gmail.com>
2022-06-24 12:16:37 +0000

    upgrade

---
+++ assets/js/musics.js
@@ -0,0 +1,15 @@
+/* HEAD */
+var swiperHead = new Swiper(".head__container", {
+  spaceBetween: 32,
+  centeredSlides: true,
+  slidesPerView: "auto",
+
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
